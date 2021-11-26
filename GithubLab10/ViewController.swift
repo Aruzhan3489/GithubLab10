6d0b6ce9ce6514a1cc9a7e03093e8817ad6ef292
Zhetenov Ralan <rollwho@gmail.com>
2021-11-26 21:24:57 +0600

    Initial Commit

---
+++ GithubLab10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubLab10
+//
+//  Created by Admin on 11/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
